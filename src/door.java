66d8ae560efa2777f742421271fb7b2ae95b4cf5
Bibek Hanal <bbekhamal26@gmail.com>
2025-02-08 10:00:41 +1100

    updating files

---
+++ src/door.java
@@ -0,0 +1,13 @@
+public class door {
+        public static void main(String [] args) {
+            int code= 2013;
+
+            if (code == 2013) {
+                System.out.println("You may enter.");
+            } else {
+                System.out.println("Please try again.");
+            }
+        }
+
+    }
+
